e71a329cc5f61e403608df18c99244c6a884628b
Alejandro Cotilla <acotilla91@gmail.com>
2019-04-14 22:18:37 -0400

    Add LabeledCellView protocol for all cells that are expected to contain a label object.

---
+++ Example/MultiCollectionView/LabeledCellView.swift
@@ -0,0 +1,13 @@
+//
+//  LabeledCellView.swift
+//  MultiCollectionView_Example
+//
+//  Created by Alejandro Cotilla on 4/14/19.
+//  Copyright © 2019 CocoaPods. All rights reserved.
+//
+
+import UIKit
+
+protocol LabeledCellView {
+    var label: UILabel! { get set }
+}
